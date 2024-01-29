5ed9e90ecaa0608c378bbbb6e5b6a42190821df2
kashiatsu <b203861@hiroshima-u.ac.jp>
2024-01-29 17:57:35 +0900

    in the middle

--- model_training.py
+++ model_training.py
@@ -19,47 +19,63 @@ ood_valid = ood_genomics_dataset.OODGenomicsDataset("data", "val_ood") #between_
 in_test = ood_genomics_dataset.OODGenomicsDataset("data", "test") # after_2016_in_test
 ood_test = ood_genomics_dataset.OODGenomicsDataset("data", "test_ood") #after_2016_ood_test
 
-train_iter = next(iter(train))
-in_valid_iter = next(iter(in_valid))
-ood_valid_iter = next(iter(ood_valid))
-in_test_iter = next(iter(in_test))
-ood_test_iter = next(iter(ood_test))
-print("train: ", train_iter.figure())
-
+# ラベル
 train_label = train.label_dict
 in_valid_label = in_valid.label_dict
 ood_valid_label = ood_valid.label_dict
 in_test_label = in_test.label_dict
 ood_test_label = ood_test.label_dict
 
-train_loder = DataLoader(train, batch_size = 32)
-in_valid_loder = DataLoader(in_valid, batch_size = 32)
-ood_valid_loder = DataLoader(ood_valid, batch_size = 32)
+train_dl = DataLoader(train, batch_size = 32)
+in_valid_dl = DataLoader(in_valid, batch_size = 32)
+ood_valid_dl = DataLoader(ood_valid, batch_size = 32)
 
+# モデルと損失関数
 
+# train用の損失関数
+def train_loss_function(train_dl, model, loss_function, epochs, optimizer):
+    a
 
-"""
-# optimizer
-optimizer = torch.optim.AdamW(params = model.parameters(),
-                              lr = 0.001, weight_decay = 0.01)
-"""
-
-# loss function
-loss_function = torch.nn.MSELoss()
-
-"""
-def run_model(model, train, in_valid, ood_valid):
-    model = model
-"""
- 
-def training_roop(train, in_valid, ood_valid): # 途中
-    train = train_loder
-    in_valid = in_valid
-    ood_valid = ood_valid
+def run_model(train_dl, in_valid_dl, ood_valid_dl, model, lr, epochs):
+    # optimizer
+    optimizer = torch.optim.AdamW(params = model.parameters(),
+                              lr = lr, weight_decay = 0.01)
+    # loss function
+    loss_function = torch.nn.MSELoss()
     
-    train_iter = next(iter(train))
-    in_valid_iter = next(iter(in_valid))
-    ood_valid_iter = next(iter(ood_valid))
+    # 損失関数の配列
+    train_losses = []
+    in_valid_losses = []
+    ood_valid_losses = []
+    
+    print("train_seq: ", next(iter(train))[0][0])
+    
+    for epoch in range(epochs):
+        # train_loss_functionとvalid_loss_functionを作る
+        train_loss = loss_function(train_dl)
+        in_valid_loss = loss_function(in_valid_dl)
+        ood_valid_loss = loss_function(ood_valid_dl)
+        
+        optimizer.zero_grad() # 勾配を初期化
+        train_loss.backward() # 勾配を計算
+        optimizer.step() # パラメータを更新
+        
+        train_losses.append(train_loss)
+        in_valid_losses.append(in_valid_loss)
+        ood_valid_losses.append(ood_valid_loss)
+        print(f"E{epoch} | train loss: {train_loss:.3f} | in valid loss: {in_valid_loss:.3f} | ood valid loss: {ood_valid_loss:.3f}")
+        
+        return train_losses, in_valid_losses, ood_valid_losses
+
+def training_roop(train_dl, in_valid_dl, ood_valid_dl): # 途中
+    seq_len = len(next(iter(train_dl))[0][0])
+    model = mymodel_cnn.model_CNN(seq_len)
+    model.to(DEVICE)
+    lr = 0.001 # 学習率
+    epochs = 10 # エポック数
+    
+    train_losses, in_valid_losses, ood_valid_losses = run_model(train_dl, in_valid_dl, ood_valid_dl, model, lr, epochs)
+    return train_losses, in_valid_losses, ood_valid_losses
     
     
-training_roop(train, in_valid, ood_valid)
+train_losses, valid_losses = training_roop(train_dl, in_valid_dl, ood_valid_dl)
--- mymodel_cnn.py
+++ mymodel_cnn.py
@@ -1,6 +1,4 @@
 import torch 
-import numpy as np
-import pandas as pd 
 from torch import nn
 
 class model_CNN(nn.Module): # 途中
@@ -10,19 +8,33 @@ class model_CNN(nn.Module): # 途中
                  kernel_size = 20):
         super().__init__()
         self.seq_len = seq_len
-        print("seq_len: ", self.seq_len)
-    
+
+        # 畳み込み層
         self.conv_net = nn.Sequential(
             nn.Conv1d(4, num_filters, kernel_size = kernel_size), # 畳み込み(長さ20の畳み込みフィルタを1000枚), ゼロパディング, stride = 1
-            nn.ReLU(inplace = True),
-            nn.MaxPool1d(kernel_size = kernel_size), # 最大プーリング(kernel_sizeは一緒じゃなくてもいい), 最大じゃなくていい
+            nn.ReLU(inplace = True)
+        )
+        
+        # Flatten + Linear(全結合層)
+        self.Flatten_Linear = nn.Sequential(
             nn.Flatten(),
             nn.Linear(num_filters * (seq_len - kernel_size + 1), 1000) # 全結合(入力: , 出力: 1000ニューロン)
         )
+
+            # nn.MaxPool1d(kernel_size = kernel_size), # 最大プーリング(kernel_sizeは一緒じゃなくてもいい), 最大じゃなくていい
+            # torch.max()
+            # nn.Flatten(),
+            # nn.Linear(num_filters * (seq_len - kernel_size + 1), 1000) # 全結合(入力: , 出力: 1000ニューロン)
+            
+        
     
     def forward(self, xb):
-        xb = xb.permute(0, 2, 1) # ? (今度決める)
+        xb = xb.permute(0, 2, 1) # 次元の並び替え(今度決める)
         out = self.conv_net(xb)
-        
+        print("out: ", out.shape())
+        out = torch.max(out, dim = 1) # 最大プーリング(各配列ごとの最大値をとる)
+        # この後にFlatten_Linearにoutを送る
+        out = self.Flatten_Linear(out)
+        print("out: ", out.shape())
         return out
     
